ef262c4c5705f0c17c1c11684aa9e878ae5e3098
Jeremy Meyer <jjman505@gmail.com>
2014-12-30 13:13:21 -0500

    user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/user/all', function(req, res) {
+  User.find({}, function(err, users) {
+    if(err) res.send(error);
+    else    res.send(users);
+  });
+});
+
+router.post('/user', function(req, res) {
+  var user = req.body;
+  console.log('user: ', user);
+
+  var newUser = new User();
+  newUser.email = user.email;
+  newUser.password = newUser.generateHash(user.password);
+
+  newUser.save(function(err) {
+    if(err) res.send({ success: false, error: err });
+    else    res.send({ success: true });
+  });
+});
+
+router.get('/user/:id', function(req, res) {
+  var userId = req.params.id;
+  console.log('userId: ', userId);
+  User.findOne(userId, function(err, user) {
+    if(err) res.send(error);
+    else    res.send(user);
+  });
+});
+
+router.put('/user/:id', function(req, res) {
+  var userId = req.params.id;
+  var user = req.body;
+  console.log('userId: ', userId);
+  console.log('user: ', user);
+  User.findOneAndUpdate({ _id: userId }, user, function(err) {
+    if(err) res.send({ success: false, error: err });
+    else    res.send({ success: true });
+  });
+});
+
+router.delete('/user/:id', function(req, res) {
+  var userId = req.params.id;
+  console.log('userId: ', userId);
+  User.remove({ _id: userId }, function(err) {
+    if(err) res.send({ success: false, error: err });
+    else    res.send({ success: true });
+  });
 });
 
 module.exports = router;
